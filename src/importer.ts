ef4a416dff7973de35185bdad6866d0c4f515362
M Ekramy <m.ajamekramy@gmail.com>
2024-02-18 10:56:32 +0330

    fix auto importer

--- src/importer.ts
+++ src/importer.ts
@@ -1,20 +1,20 @@
 import { App } from "vue";
 
-export interface AutoComPattern {
+interface importPattern {
     files: Record<string, any>;
     prefix: string;
 }
 
-export function AutoComponent(patterns: AutoComPattern[]) {
+export function AutoComponent(patterns: importPattern[]) {
     return {
         install(app: App) {
             for (const pattern of patterns) {
-                Object.entries(pattern.files).forEach(([path, com]) => {
-                    let name = path.replace(/^.*[\\/]/, "");
-                    name = name.replace(".vue", "");
-                    if (name != "") {
-                        app.component("I" + name, com);
-                    }
+                Object.entries(pattern.files).forEach(([path, c]) => {
+                    const com = (c as any)?.default || c as any;
+                    const name = path.replace(/^.*[\\/]/, "").replace(".vue", "")
+                    if (com && name != ""){
+                        app.component(pattern.prefix + name, com);
+                    } 
                 });
             }
         },
